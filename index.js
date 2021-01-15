dad4cdc45843a79437f2ac691f37a0ec7f1d5788
its_kedar <kedar.bhujade6@gmail.com>
2021-01-15 20:14:20 +0530

    no changes

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+let textValue = document.querySelector("#insert");
+let addbtn = document.querySelector("#add");
 let month = [" Jan "," Feb "," Mar "," April "," May "," June "," July "," Aug "," Sept "," Oct "," Nov "," Dec "];
 let date = new Date();
 let fulldate = date.getDate() + month[date.getMonth()] + date.getFullYear();
 document.querySelector(".date").innerHTML=fulldate;
+
+
+addbtn.addEventListener("click", function(){
+    
+
+  });
