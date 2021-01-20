e7acecd3b1ba1a9404e83b4cb5a4f6e801dca06a
its_kedar <kedar.bhujade6@gmail.com>
2021-01-20 21:43:57 +0530

    logic for insert checkbox and text in list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let textValue = document.querySelector("#insert");
 let addbtn = document.querySelector("#add");
+let list = document.querySelector(".list");
 let month = [" Jan "," Feb "," Mar "," April "," May "," June "," July "," Aug "," Sept "," Oct "," Nov "," Dec "];
 let date = new Date();
 let fulldate = date.getDate() + month[date.getMonth()] + date.getFullYear();
@@ -7,6 +8,12 @@ document.querySelector(".date").innerHTML=fulldate;
 
 
 addbtn.addEventListener("click", function(){
-    
-
+  var li = document.createElement("li");
+  var checkbox= document.createElement("input");
+  checkbox.type="checkbox";
+  var t = document.createTextNode(textValue.value);
+  li.appendChild(checkbox);
+  li.appendChild(t);
+  document.querySelector("ul").appendChild(li);
+  textValue.value ="";
   });
